d31b5593d101502e136e0f525e92107fc91a3ec3
niklasekstein <Nicke_ponken13@hotmail.com>
2022-08-20 19:20:43 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Botek-Test';
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+      'username' : new FormControl<string|null>(null, Validators.required),
+      'email' : new FormControl<string|null>(null, [Validators.required, Validators.email]),
+      // 'gender' : new FormControl<string|null>('male', Validators.required)
+    })
+    });
+  }
+  onSubmit() {
+    //if (this.signupForm.value.username) {
+      console.log(this.signupForm.value.username.length)
+    //}
+    //if (this.signupForm.value.email) {
+      console.log(this.signupForm.value.email.length)
+    //}
+
+    //if (this.signupForm.value.email) {
+      console.log(this.signupForm.value.email.length)
+    //}
+  }
 }
